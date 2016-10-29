9e298ae3df3341fcbb2f560623e3b7ca130f814f
tioigor96 <tioigor96@users.noreply.github.com>
2016-10-29 19:06:17 +0200

    code refactoring
    correct coding style wt CLion

--- mypasswd.c
+++ mypasswd.c
@@ -24,7 +24,7 @@
 #define MAX_QUERY_LEN 256
 
 #define __CIPHER "$1$"
-#define __SALT_len	8
+#define __SALT_len    8
 
 #define __DBUSER "DB_USER_U_SHOULD_KNOW_IT"
 #define __DBPASS "I_DON'T_KNOW_UR_DB_PASS_SER"
@@ -37,75 +37,85 @@
 
 
 bool getUserInfo(char **cuser, int *cuid);
+
 bool getConnectMysql(MYSQL **sock);
+
 bool do_MySQL_SELECT_PASSWORD(MYSQL *sock, char **password, char *user, int cuid);
-bool do_MySQL_UPDATE_PASSWORD(MYSQL *sock, char **passwdhash, char* user, int cuid);
-char* make_rand_salt(size_t len);
-char* make_hashed_pass(char *new_password);
+
+bool do_MySQL_UPDATE_PASSWORD(MYSQL *sock, char **passwdhash, char *user, int cuid);
+
+char *make_rand_salt(size_t len);
+
+char *make_hashed_pass(char *new_password);
+
 bool chk_passwd(char *password, char *password2check);
+
 bool chk_new_passwd(char *old_passwd);
 
-int main(int argc, char **argv)
-{
-	/* attrib
-	 * char:
-	 *  - *cuser: current_username
-	 *  - *passwd: selected password
-	 *  - tmpstr1: tmp string 1
-	 *
-	 * int:
-	 *  - ciud: current user id
-	 *
-	 * MYSQL:
-	 *  - conn: connection to db;
-	 */
+int main(int argc, char **argv) {
+    /* attrib
+     * char:
+     *  - *cuser: current_username
+     *  - *passwd: selected password
+     *  - tmpstr1: tmp string 1
+     *
+     * int:
+     *  - ciud: current user id
+     *
+     * MYSQL:
+     *  - conn: connection to db;
+     */
 
-	char *cuser, *passwd;
-	char tmpstr1[MAX_QUERY_LEN];
-	int cuid=0;
-	MYSQL *conn=NULL;
+    char *cuser, *passwd;
+    char tmpstr1[MAX_QUERY_LEN];
+    int cuid = 0;
+    MYSQL *conn = NULL;
 
-	if(!getUserInfo(&cuser,&cuid)){								//getUID
-		printf("Error while getting user information. Exit\n");
-		exit(EXIT_FAILURE);}
+    if (!getUserInfo(&cuser, &cuid)) {                                //getUID
+        printf("Error while getting user information. Exit\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if(!getConnectMysql(&conn)){								//mysqlconn
-                printf("Error while getting MySQL connection.\n%s.\nExit\n",mysql_error(conn));
-                exit(EXIT_FAILURE);}
+    if (!getConnectMysql(&conn)) {                                //mysqlconn
+        printf("Error while getting MySQL connection.\n%s.\Exit\n", mysql_error(conn));
+        exit(EXIT_FAILURE);
+    }
 
-	if(!do_MySQL_SELECT_PASSWORD(conn, &passwd, cuser, cuid)){
-			printf("Error while SELECT from MySQL.\nERROR:%s",passwd);
-			exit(EXIT_FAILURE);}
+    if (!do_MySQL_SELECT_PASSWORD(conn, &passwd, cuser, cuid)) {
+        printf("Error while SELECT from MySQL.\nERROR:%s", passwd);
+        exit(EXIT_FAILURE);
+    }
 
-	//check password
-	printf("Changing password for %s.\n",cuser);
-	strcpy(tmpstr1,getpass("(current) UNIX password:"));
+    //check password
+    printf("Changing password for %s.\n", cuser);
+    strcpy(tmpstr1, getpass("(current) UNIX password:"));
 
-	if(strlen(tmpstr1)==0 || !chk_passwd(passwd,tmpstr1) || !chk_new_passwd(tmpstr1)){
-		printf("passwd: Authentication token manipulation error\npasswd: password unchanged\n");
-		exit(EXIT_FAILURE);
-	}
+    if (strlen(tmpstr1) == 0 || !chk_passwd(passwd, tmpstr1) || !chk_new_passwd(tmpstr1)) {
+        printf("passwd: Authentication token manipulation error\npasswd: password unchanged\n");
+        exit(EXIT_FAILURE);
+    }
 
-	//create new hashed password
-	passwd=make_hashed_pass(tmpstr1);
+    //create new hashed password
+    passwd = make_hashed_pass(tmpstr1);
 
-	//reset connnection
-	mysql_close(conn);
-	if(!getConnectMysql(&conn)){								//mysqlconn
-	                printf("Error while getting MySQL connection.\n%s.\nExit\n",mysql_error(conn));
-	                exit(EXIT_FAILURE);}
+    //reset connnection
+    mysql_close(conn);
+    if (!getConnectMysql(&conn)) {                                //mysqlconn
+        printf("Error while getting MySQL connection.\n%s.\Exit\n", mysql_error(conn));
+        exit(EXIT_FAILURE);
+    }
 
-	//update
-	if(!do_MySQL_UPDATE_PASSWORD(conn, &passwd, cuser, cuid)){
-		printf("Error while UPDATE PASSWORD for MySQL.\nERROR:%s",passwd);
-		exit(EXIT_FAILURE);
-	}
+    //update
+    if (!do_MySQL_UPDATE_PASSWORD(conn, &passwd, cuser, cuid)) {
+        printf("Error while UPDATE PASSWORD for MySQL.\nERROR:%s", passwd);
+        exit(EXIT_FAILURE);
+    }
 
-	mysql_close(conn);
+    mysql_close(conn);
 
-	printf("passwd: password updated successfully\n");
+    printf("passwd: password updated successfully\n");
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
 
 /**
@@ -113,24 +123,22 @@ int main(int argc, char **argv)
  *	@param Pointer to username
  *	@param Pointer to UID
  */
-bool getUserInfo(char **cuser, int *cuid)
-{
-	register struct passwd *user=getpwuid(geteuid());
+bool getUserInfo(char **cuser, int *cuid) {
+    register struct passwd *user = getpwuid(geteuid());
 
-	*cuser=user->pw_name;
-	*cuid=(int)user->pw_uid;
+    *cuser = user->pw_name;
+    *cuid = (int) user->pw_uid;
 
-	return (cuser && *cuid);
+    return (cuser && *cuid);
 }
 
 /**
  *	Get init the MySQL Connection.
  *	@param Pointer to main Variable
  */
-bool getConnectMysql(MYSQL **sock)
-{
-	*sock=mysql_init(NULL);
-	return mysql_real_connect(*sock, __HOST, __DBUSER, __DBPASS, __DBNAME, 0, __SOCK, 0);
+bool getConnectMysql(MYSQL **sock) {
+    *sock = mysql_init(NULL);
+    return mysql_real_connect(*sock, __HOST, __DBUSER, __DBPASS, __DBNAME, 0, __SOCK, 0);
 }
 
 /**
@@ -142,25 +150,25 @@ bool getConnectMysql(MYSQL **sock)
  * @param Current username
  * @param Current uid
  */
-bool do_MySQL_SELECT_PASSWORD(MYSQL *sock, char **password, char *user, int cuid)
-{
-	//MYSQL_RES *result;
-	MYSQL_ROW row;
-	char query[MAX_QUERY_LEN];
+bool do_MySQL_SELECT_PASSWORD(MYSQL *sock, char **password, char *user, int cuid) {
+    //MYSQL_RES *result;
+    MYSQL_ROW row;
+    char query[MAX_QUERY_LEN];
 
-	sprintf(query,__SELECT_QUERY,user,cuid);
+    sprintf(query, __SELECT_QUERY, user, cuid);
 
-	if(mysql_query(sock,query)){
-		*password=strcat("MALFORMED SELECT QUERY. ABORT.\n",mysql_error(sock));
-		return false;}
+    if (mysql_query(sock, query)) {
+        *password = strcat("MALFORMED SELECT QUERY. ABORT.\n", mysql_error(sock));
+        return false;
+    }
 
-	MYSQL_RES *result=mysql_use_result(sock);
-	row=mysql_fetch_row(result);
+    MYSQL_RES *result = mysql_use_result(sock);
+    row = mysql_fetch_row(result);
 
-	*password=row[0];
+    *password = row[0];
 
-	//ci sarebbe da fare la free, ma non la mettiamo causa perdita corrispondenza memoria
-	return true;
+    //ci sarebbe da fare la free, ma non la mettiamo causa perdita corrispondenza memoria
+    return true;
 
 
 }
@@ -174,17 +182,17 @@ bool do_MySQL_SELECT_PASSWORD(MYSQL *sock, char **password, char *user, int cuid
  * @param Current username
  * @param Current uid
  */
-bool do_MySQL_UPDATE_PASSWORD(MYSQL *sock, char **passwdhash, char *user, int cuid)
-{
-	char query[MAX_QUERY_LEN];
+bool do_MySQL_UPDATE_PASSWORD(MYSQL *sock, char **passwdhash, char *user, int cuid) {
+    char query[MAX_QUERY_LEN];
 
-	sprintf(query,__UPDATE_QUERY,*passwdhash,user,cuid);
+    sprintf(query, __UPDATE_QUERY, *passwdhash, user, cuid);
 
-	if(mysql_query(sock,query)){
-			*passwdhash=strcat("MALFORMED UPDATE QUERY. ABORT.\n",mysql_error(sock));
-			return false;}
+    if (mysql_query(sock, query)) {
+        *passwdhash = strcat("MALFORMED UPDATE QUERY. ABORT.\n", mysql_error(sock));
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 
@@ -194,9 +202,8 @@ bool do_MySQL_UPDATE_PASSWORD(MYSQL *sock, char **passwdhash, char *user, int cu
  * @param Hashed password
  * @param Password to check.
  */
-bool chk_passwd(char *password, char *password2check)
-{
-	return (strcmp(password,crypt(password2check,password))==0)?true:false;
+bool chk_passwd(char *password, char *password2check) {
+    return (strcmp(password, crypt(password2check, password)) == 0) ? true : false;
 }
 
 
@@ -205,31 +212,31 @@ bool chk_passwd(char *password, char *password2check)
  *
  * @param string contain old password
  */
-bool chk_new_passwd(char *old_passwd)
-{
-	char tmp1[MAX_QUERY_LEN], tmp2[MAX_QUERY_LEN];
-	int i;
-	for(i=0; i<3; i++){
-		strcpy(tmp1,getpass("Enter new UNIX password:"));
-		strcpy(tmp2,getpass("Retype new UNIX password:"));
+bool chk_new_passwd(char *old_passwd) {
+    char tmp1[MAX_QUERY_LEN], tmp2[MAX_QUERY_LEN];
+    int i;
+    for (i = 0; i < 3; i++) {
+        strcpy(tmp1, getpass("Enter new UNIX password:"));
+        strcpy(tmp2, getpass("Retype new UNIX password:"));
 
-		if(strcmp(tmp1,tmp2)!=0){
-			printf("Sorry, passwords do not match\n");
-			return false;
-		}else{
-			if(!strlen(tmp1)|| !strlen(tmp2)){
-				printf("No password supplied\n");
-			}else{
-				if(strcmp(tmp1,old_passwd)==0){
-					printf("Password unchanged\n");
-				}else{
-					strcpy(old_passwd,tmp1);
-					return true;
-				}
-			}
-		}
-	}
-	return false;
+        if (strcmp(tmp1, tmp2) != 0) {
+            printf("Sorry, passwords do not match\n");
+            return false;
+        } else {
+            if (!strlen(tmp1) || !strlen(tmp2)) {
+                printf("No password supplied\n");
+            } else {
+                if (strcmp(tmp1, old_passwd) == 0) {
+                    printf("Password unchanged\n");
+                } else {
+                    strcpy(old_passwd, tmp1);
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 
@@ -238,18 +245,17 @@ bool chk_new_passwd(char *old_passwd)
  *
  * @param How long would you like the salt?
  */
-char* make_rand_salt(size_t len)
-{
-	char *salt=(char*)malloc(sizeof(char)*((int)len));
-	char *charset="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?@[\\]^_`{|}";
-	int size=strlen(charset);
+char *make_rand_salt(size_t len) {
+    char *salt = (char *) malloc(sizeof(char) * ((int) len));
+    char *charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?@[\\]^_`{|}";
+    int size = strlen(charset);
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	for(int i=0; i<len; i++)
-		salt[i]=charset[rand()%size];
+    for (int i = 0; i < len; i++)
+        salt[i] = charset[rand() % size];
 
-	return salt;
+    return salt;
 }
 
 
@@ -258,11 +264,9 @@ char* make_rand_salt(size_t len)
  *
  *	@param password to encode
  */
-char* make_hashed_pass(char *new_password)
-{
-	char easysalt[PASSWD_LENGTH];
-	sprintf(easysalt,"%s%s",__CIPHER,make_rand_salt(__SALT_len));
+char *make_hashed_pass(char *new_password) {
+    char easysalt[PASSWD_LENGTH];
+    sprintf(easysalt, "%s%s", __CIPHER, make_rand_salt(__SALT_len));
 
-	return crypt(new_password,easysalt);
+    return crypt(new_password, easysalt);
 }
-
