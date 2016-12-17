c369b66f56f328cc53d53c8a0feeff1349714a16
tioigor96 <tioigor96@users.noreply.github.com>
2016-12-17 11:40:50 +0100

    corrected GNUv3 license

--- mypasswd.c
+++ mypasswd.c
@@ -1,6 +1,18 @@
 /**
  *	Filippo Callegari - 29 october 2016
- *	This suorce is under GNU v3 license.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *	Compile this code with this command:
  *	gcc name_of_source.c -lcrypt -o name_of_executable $(mysql_config --libs --cflags)
